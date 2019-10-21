cefa12eb042b481df8d93676dec6461a4915667e
M. Fatih MAR <mfatihmar@gmail.com>
2019-10-21 21:35:08 +0100

    Update Deck Theme

--- Multiplayer-Architecture/theme.jsx
+++ Multiplayer-Architecture/theme.jsx
@@ -2,6 +2,25 @@ export default {
 	styles: {
 		Slide: {
 			padding: "0 10%"
+		},
+		h1: {
+			backgroundColor: "red",
+			color: "white",
+			padding: "0 16px"
+		},
+		h2: {
+			backgroundColor: "#017ACB",
+			color: "white",
+			padding: "0 16px"
+		},
+		CodeSurfer: {
+			title: {
+				span: {
+					backgroundColor: "#017ACB",
+					color: "white",
+					padding: "0 16px"
+				}
+			}
 		}
 	}
 };
