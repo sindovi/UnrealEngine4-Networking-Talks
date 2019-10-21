2b77437f65385bb0a98497922608dd813867d4dd
M. Fatih MAR <mfatihmar@gmail.com>
2019-10-21 23:43:38 +0100

    Update EngineFlow.cpp

--- Multiplayer-Architecture/snippets/EngineFlow.cpp
+++ Multiplayer-Architecture/snippets/EngineFlow.cpp
@@ -9,7 +9,34 @@ int32 GuardedMain(const TCHAR* CmdLine)
 			EngineClass = StaticLoadClass(UGameEngine::StaticClass(), nullptr, *GameEngineClassName);
 			GEngine = NewObject<UEngine>(GetTransientPackage(), EngineClass);
 			GEngine->Init(this);
+				UGameEngine::Init(InEngineLoop);
+					UEngine::Init(InEngineLoop);
+						EngineSubsystemCollection.Initialize();
+						InitializeRunningAverageDeltaTime();
+						AddToRoot();
+						FCoreUObjectDelegates::GetPreGarbageCollectDelegate().AddStatic(UEngine::PreGarbageCollect);
+						LoadConfig();
+						InitializeObjectReferences();
+						const UGeneralProjectSettings& ProjectSettings = *GetDefault<UGeneralProjectSettings>();
+						FNetworkVersion::SetProjectVersion(*ProjectSettings.ProjectVersion);
+						OnTravelFailure().AddUObject(this, &UEngine::HandleTravelFailure);
+						OnNetworkFailure().AddUObject(this, &UEngine::HandleNetworkFailure);
+						OnNetworkLagStateChanged().AddUObject(this, &UEngine::HandleNetworkLagStateChanged);
+						FEngineAnalytics::Initialize();
+						EngineStats.Add(XXX);
+					GNetworkProfiler.EnableTracking(true);
+					GetGameUserSettings()->LoadSettings();
+					GetGameUserSettings()->ApplyNonResolutionSettings();
+					FSoftClassPath GameInstanceClassName = GetDefault<UGameMapsSettings>()->GameInstanceClass;
+					UClass* GameInstanceClass = LoadObject<UClass>(NULL, *GameInstanceClassName.ToString());
+					GameInstance = NewObject<UGameInstance>(this, GameInstanceClass);
+					GameInstance->InitializeStandalone();
+					UGameViewportClient* ViewportClient = NewObject<UGameViewportClient>(this, GameViewportClientClass);
+					ViewportClient->Init(*GameInstance->GetWorldContext(), GameInstance);
+					GameInstance->GetWorldContext()->GameViewport = ViewportClient;
 			GEngine->Start();
+				UGameEngine::Start();
+					GameInstance->StartGameInstance();
 	while (!GIsRequestingExit) { EngineTick(); }
 	~EngineLoopCleanupGuard() { EngineExit(); }
 	return ErrorLevel;
