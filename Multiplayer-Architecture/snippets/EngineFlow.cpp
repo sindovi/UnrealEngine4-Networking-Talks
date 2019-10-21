d776676a4cbe29e7b34faa6cec94ca6a67a70c3a
M. Fatih MAR <mfatihmar@gmail.com>
2019-10-21 14:02:36 +0100

    Update EngineFlow.cpp

--- Multiplayer-Architecture/snippets/EngineFlow.cpp
+++ Multiplayer-Architecture/snippets/EngineFlow.cpp
@@ -1,5 +1,15 @@
+FEngineLoop	GEngineLoop;
 int32 GuardedMain(const TCHAR* CmdLine)
 	int32 ErrorLevel = EnginePreInit(CmdLine);
+		FEngineLoop::PreInit(CmdLine);
+			// Paths, Modules, Slate, Platform, Application, CommandLet, Plugins etc.
 	ErrorLevel = EngineInit();
+		FEngineLoop::Init()
+			GConfig->GetString(TEXT("/Script/Engine.Engine"), TEXT("GameEngine"), GameEngineClassName, GEngineIni);
+			EngineClass = StaticLoadClass(UGameEngine::StaticClass(), nullptr, *GameEngineClassName);
+			GEngine = NewObject<UEngine>(GetTransientPackage(), EngineClass);
+			GEngine->Init(this);
+			GEngine->Start();
 	while (!GIsRequestingExit) { EngineTick(); }
+	~EngineLoopCleanupGuard() { EngineExit(); }
 	return ErrorLevel;
